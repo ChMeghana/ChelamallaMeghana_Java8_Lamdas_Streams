c78b8bfae5b8368725ba9c39963a4e40b03c523b
Meghana <chelamallameghana2099@gmail.com>
2020-07-26 18:35:12 +0530

    epam_task_lamdas_streams

--- com.lamdas/src/main/java/com/lamdas/com/lamdas/IsPalindrome.java
+++
@@ -1,7 +0,0 @@
-package com.lamdas.com.lamdas;
-
-public class IsPalindrome {	
-	public static boolean check(String str) {
-		return str.equals(new StringBuilder(str).reverse().toString());
-	}
-}
---
+++ com.lamdas_streams/src/main/java/com/lamdas_streams/com/lamdas_streams/App.java
@@ -0,0 +1,13 @@
+package com.lamdas_streams.com.lamdas_streams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
